b8992325bcd41cf377faec7d8aa1754f843e7858
Hyago Philip <36174848+hyago1@users.noreply.github.com>
2026-06-15 00:12:40 -0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,7 @@
+// Dentro do seu arquivo ./supabase.js
+
+const URL_DO_BANCO = 'https://gtmbjunwwgwibzrwumfh.supabase.co';
+const CHAVE_DO_BANCO = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0bWJqdW53d2d3aWJ6cnd1bWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0ODA3NDQsImV4cCI6MjA5NzA1Njc0NH0.VQehZvKNsT0TJ-CaB3gb0JUxf01BSi5C1EOYmcPTqEE';
+
+// Aqui usamos a biblioteca global (supabase) para criar o SEU cliente customizado (window.supabaseClient)
+window.supabaseClient = supabase.createClient(URL_DO_BANCO, CHAVE_DO_BANCO);
